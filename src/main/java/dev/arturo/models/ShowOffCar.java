bd3cfa29a5b10612d2732fec60f503ca4e1b20e9
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-02 09:18:33 +0200

    refacting my whole code

--- src/main/java/dev/arturo/models/ShowOffCar.java
+++ src/main/java/dev/arturo/models/ShowOffCar.java
@@ -1,9 +1,5 @@
 package dev.arturo.models;
 
-import dev.arturo.models.enumatributes.CarColors;
-import dev.arturo.models.enumatributes.CarType;
-import dev.arturo.models.enumatributes.Foul;
-
 public class ShowOffCar {
 
     public String brag(CarAtributes carAtributes){
@@ -20,14 +16,5 @@ public class ShowOffCar {
         sb.append("Actual speed: ").append(carAtributes.getActualSpeed()).append("\n");
 
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        CarAtributes carAtributes = new CarAtributes("Toyota", 2025, 5.0, Foul.gasoline, CarType.city, 9, 10, 220, CarColors.purple, 0);
-
-        ShowOffCar showOffCar = new ShowOffCar();
-
-        System.out.println(showOffCar.brag(carAtributes));
-
     }
 }
