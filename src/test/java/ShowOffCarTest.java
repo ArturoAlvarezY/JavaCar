08bbd750c54dc4ecfb9316b79ccf340d8dd13db5
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-02 09:17:18 +0200

    making the ShowOffCarTests

---
+++ src/test/java/ShowOffCarTest.java
@@ -0,0 +1,20 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import dev.arturo.models.CarAtributes;
+import dev.arturo.models.ShowOffCar;
+import dev.arturo.models.enumatributes.CarColors;
+import dev.arturo.models.enumatributes.CarType;
+import dev.arturo.models.enumatributes.Foul;
+
+public class ShowOffCarTest {
+     CarAtributes carAtributes = new CarAtributes("Toyota", 2025, 5.0, Foul.gasoline, CarType.city, 9, 10, 220, CarColors.purple, 0);
+
+        ShowOffCar showOffCar = new ShowOffCar();
+    @Test
+    void testBrag() {
+        assertThat(showOffCar.brag(carAtributes), is( "Car Brand: Toyota\nModel: 2025\nMotor: 5.0\nFoul kind: gasoline\nCarType: city\nDoors: 9\nSits: 10\nSpeed: 220\nColor: purple\nActual speed: 0\n"));
+    }
+}
