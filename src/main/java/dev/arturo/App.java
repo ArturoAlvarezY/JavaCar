5aaccd62f32fecef175059a7e68d398821ab82fe
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-08-02 09:27:02 +0200

    adding main method

--- src/main/java/dev/arturo/App.java
+++ src/main/java/dev/arturo/App.java
@@ -1,17 +1,34 @@
 package dev.arturo;
 
-/**
- * Hello world!
- */
+import dev.arturo.models.CarAcelerate;
+import dev.arturo.models.CarAtributes;
+import dev.arturo.models.enumatributes.CarColors;
+import dev.arturo.models.enumatributes.CarType;
+import dev.arturo.models.enumatributes.Foul;
+
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+   
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CarAtributes carAtributes = new CarAtributes("Toyota", 2025, 5.0, Foul.gasoline, CarType.city, 5, 5, 140, CarColors.purple, 0);
+
+        CarAcelerate carAcelerate = new CarAcelerate();
+
+        carAtributes.setActualSpeed(100);
+
+        System.out.println(carAtributes.getActualSpeed() + "km/h");
+
+        carAtributes.setActualSpeed(120);
+
+        System.out.println(carAtributes.getActualSpeed() + "km/h");
+
+        carAtributes.setActualSpeed(70);
+
+        System.out.println(carAtributes.getActualSpeed() + "km/h");
+
+
+        System.out.println(carAcelerate.breakMethod(carAtributes));
     }
 }
